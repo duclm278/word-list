b9dc30e3984f2b2ed506d589caee82bcacd52eab
Duc Le <palered548@gmail.com>
2022-03-24 22:16:26 +0700

    Try Cloze Deletion

--- cambridge/extract-data.py
+++ cambridge/extract-data.py
@@ -1,108 +1,158 @@
 import os
+import spacy
 import requests
 from bs4 import BeautifulSoup
+from helpers import Setup
+from lemminflect import getAllInflections
 
-max_quotes = 2
+max_quotes = 5
+get_audios = False
 
-def save_audio(filename, url):
-    response = requests.get(url)
-    if response.status_code == 200:
-        if not os.path.exists("media"):
-            os.makedirs("media")
-        with open(f"media/{filename}", "wb") as file:
-            file.write(response.content)
+setup = Setup()
+nlp = spacy.load("en_core_web_sm")
 
-def save_data(data):
-    with open("data.txt", "a") as file:
-        file.write(data)
+def main():
+    setup.set_proxies()
 
-def search_word(n):
-    url = f"https://www.englishprofile.org/american-english/words/usdetail/{n}"
-    response = requests.get(url)
-    doc = BeautifulSoup(response.content, "html.parser")
+    try:
+        first = int(input("First: "))
+        final = int(input("Final: "))
+    except:
+        exit()
 
-    details = doc.find_all("div", attrs={"class": "evp_details"})[-1]
+    with open("data.txt", "a", encoding="utf-8") as file:
+        for i in range(first, final + 1):
+            url = f"https://www.englishprofile.org/american-english/words/usdetail/{i}"
+            response = requests.get(url, headers=setup.headers, proxies=setup.proxies)
+
+            # Pass the page to Beautiful Soup
+            html = response.content
+            headword, data = get_data(html)
+
+            file.write(data)
+            print(f"{i}. {headword}")
+
+    print("=> Extract complete!")
+
+def get_data(html):
+    soup = BeautifulSoup(html, "html.parser")
+
+    details = soup.find_all("div", attrs={"class": "evp_details"})[-1]
     headword = details.find("span", attrs={"class": "headword"}).string
+
+    data = ""
+    blocks = []
     entries = details.find_all(recursive=False)
     for entry in entries:
-        if entry["class"] == ["pos_section"]:
-            audio = ""
-            audio_tag = entry.find("audio")
-            if audio_tag != None:
+        if "sense" in entry["class"]:
+            blocks.append({
+                "header": None,
+                "sense": entry
+            })
+        if "pos_section" in entry["class"]:
+            header = entry.find("div", attrs={"class": "pos_header"})
+            for sense in entry.find_all("div", attrs={"class": "sense"}):
+                blocks.append({
+                    "header": header,
+                    "sense": sense
+                })
+    
+    data += get_senses(headword, blocks)
+    return headword, data
+
+def get_senses(headword, blocks):
+    data = ""
+    for block in blocks:
+        audio = ""
+        written = ""
+
+        header = block["header"]
+        if get_audios and header:
+            audio_tag = header.find("audio")
+            if audio_tag:
                 audio_src = audio_tag.find("source")["src"]
                 audio_url = "https://www.englishprofile.org" + audio_src
                 filename = os.path.basename(audio_url).lower()
                 save_audio(filename, audio_url)
                 audio = f"[sound:{filename}]"
 
-            written = ""
-            written_tag = entry.find("span", attrs={"class": "written"})
-            if written_tag != None:
+            written_tag = header.find("span", attrs={"class": "written"})
+            if written_tag:
                 written = written_tag.string
 
-            subentries = entry.select("div.info.sense")
-            for subentry in subentries:
-                title = subentry.find("div", attrs={"class": "sense_title"}).string
-                label = subentry.find("span", attrs={"class": "label"}).string
-                definition = subentry.find("span", attrs={"class": "definition"}).string
+        sense = block["sense"]
+        title = sense.find("div", attrs={"class": "sense_title"}).string
+        label = sense.find("span", attrs={"class": "label"}).string
+        definition = sense.find("span", attrs={"class": "definition"}).string
 
-                example = subentry.find("div", attrs={"class": "example"})
-                if example != None:
-                    example_quotes = example.find_all("p", attrs={"class": "blockquote"})
-                else:
-                    example_quotes = None
-
-                learner = subentry.find("div", attrs={"class": "learner"})
-                if learner != None:
-                    learner_quotes = learner.find_all("p", attrs={"class": "learnerexamp"})
-                else:
-                    learner_quotes = None
-
-                sentences = ""
-                for i in range(max_quotes):
-                    if example_quotes:
-                        sentences += "\t" + example_quotes.pop(0).get_text().strip()
-                    elif learner_quotes:
-                        sentences += "\t" + learner_quotes.pop(0).get_text().strip()
-                    else:
-                        sentences += "\t"
-
-                data = f"{title}\t{written}\t{audio}\t[{label}] {definition}{sentences}\n"
-                save_data(data)
+        example = sense.find("div", attrs={"class": "example"})
+        if example:
+            example_quotes = example.find_all("p", attrs={"class": "blockquote"})
         else:
-            audio = ""
-            written = ""
+            example_quotes = None
 
-            title = entry.find("div", attrs={"class": "sense_title"}).string
-            label = entry.find("span", attrs={"class": "label"}).string
-            definition = entry.find("span", attrs={"class": "definition"}).string
+        learner = sense.find("div", attrs={"class": "learner"})
+        if learner:
+            learner_quotes = learner.find_all("p", attrs={"class": "learnerexamp"})
+        else:
+            learner_quotes = None
 
-            example = entry.find("div", attrs={"class": "example"})
-            if example != None:
-                example_quotes = example.find_all("p", attrs={"class": "blockquote"})
+        form_dict = getAllInflections(headword)
+        forms = {y for x in form_dict.values() for y in x}
+        if not forms:
+            forms = {headword}
+
+        sentences = ""
+        for i in range(max_quotes):
+            if example_quotes:
+                sentence = example_quotes.pop(0).get_text().strip()
+                sentences += "\t" + sentence + "\t" + cloze_sentence(sentence, forms)
+            elif learner_quotes:
+                sentence = learner_quotes.pop(0).get_text().strip()
+                sentences += "\t" + sentence + "\t" + cloze_sentence(sentence, forms)
             else:
-                example_quotes = None
+                sentences += "\t\t"
 
-            learner = entry.find("div", attrs={"class": "learner"})
-            if learner != None:
-                learner_quotes = learner.find_all("p", attrs={"class": "learnerexamp"})
-            else:
-                learner_quotes = None
+        cloze = cloze_word(headword)
+        data += f"{headword}\t{cloze}\t{title}\t{written}\t{audio}\t[{label}] {definition}{sentences}\n"
 
-            sentences = ""
-            for i in range(max_quotes):
-                if example_quotes:
-                    sentences += "\t" + example_quotes.pop(0).get_text().strip()
-                elif learner_quotes:
-                    sentences += "\t" + learner_quotes.pop(0).get_text().strip()
-                else:
-                    sentences += "\t"
+    return data
 
-            data = f"{title}\t{written}\t{audio}\t[{label}] {definition}{sentences}\n"
-            save_data(data)
+def cloze_sentence(sentence, forms):
+    doc = nlp(sentence)
 
-    return headword
+    processed = ""
+    for token in doc:
+        text = token.text
+        if text.lower() in forms:
+            processed += "<b>" + cloze_word(text) + "</b>"
+            processed += token.whitespace_
+        else:
+            processed += token.text_with_ws
 
-for i in range(1, 6757):
-    print(f"{i}. {search_word(i)}")
-print("Extract complete!")
+    return processed
+
+def cloze_word(text):
+    processed = ""
+    if len(text) <= 2:
+        # processed += "[...]"
+        processed += " ".join(["_"] * len(text))
+    elif len(text) <= 4:
+        # processed += text[0] + "[...]"
+        processed += text[0] + " _" * (len(text) - 1)
+    else:
+        # processed += text[0] + "[...]" + text[-1]
+        processed += text[0] + " _" * (len(text) - 2) + " " + text[-1]
+    
+    return processed
+
+def save_audio(filename, url):
+    response = requests.get(url, headers=setup.headers, proxies=setup.proxies)
+    if response.status_code == 200:
+        if not os.path.exists("media"):
+            os.makedirs("media")
+        with open(f"media/{filename}", "wb") as file:
+            file.write(response.content)
+
+if __name__ == "__main__":
+    main()
---
+++ cambridge/helpers.py
@@ -0,0 +1,14 @@
+class Setup:
+    def __init__(self):
+        self.headers = {
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:80.0) Gecko/20100101 Firefox/80.0"
+        }
+        self.proxies = {}
+    
+    def set_proxies(self):
+        username = input("Proxy Username: ")
+        password = input("Proxy Password: ")
+        self.proxies = {
+            "http": f"http://{username}:{password}@p.webshare.io:80/",
+            "https": f"http://{username}:{password}@p.webshare.io:80/",
+        }
