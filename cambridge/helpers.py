e3dba5e1c7cd68132a83852984a604375d4f3ea5
Duc Le <palered548@gmail.com>
2022-04-05 16:30:01 +0700

    Minor changes

--- cambridge/helpers.py
+++ cambridge/helpers.py
@@ -1,4 +1,4 @@
-class Setup:
+class Setup():
     def __init__(self):
         self.headers = {
             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:80.0) Gecko/20100101 Firefox/80.0"
